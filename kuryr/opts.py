7301bf21222b94c77f9ff31ebbd9d17b5b734c5a
Mohammad Banikazemi <mb@us.ibm.com>
2015-09-14 00:33:32 -0400

    Adding configurations for Kuryr
    Partially Implements blueprint kuryr-config
    
    Change-Id: I66738833ce14275f4ca647724bd695a69c7c0380

---
+++ kuryr/opts.py
@@ -0,0 +1,48 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+__all__ = [
+    'list_kuryr_opts',
+]
+
+import copy
+import itertools
+
+import kuryr.common.config
+
+
+_kuryr_opts = [
+    (None, list(itertools.chain(
+        kuryr.common.config.core_opts))),
+    ('neutron_client', kuryr.common.config.neutron_opts),
+    ('keystone_client', kuryr.common.config.keystone_opts),
+]
+
+
+def list_kuryr_opts():
+    """Return a list of oslo_config options available in Kuryr service.
+
+    Each element of the list is a tuple. The first element is the name of the
+    group under which the list of elements in the second element will be
+    registered. A group name of None corresponds to the [DEFAULT] group in
+    config files.
+
+    This function is also discoverable via the 'kuryr' entry point under
+    the 'oslo_config.opts' namespace.
+
+    The purpose of this is to allow tools like the Oslo sample config file
+    generator to discover the options exposed to users by Kuryr.
+
+    :returns: a list of (group_name, opts) tuples
+    """
+
+    return [(k, copy.deepcopy(o)) for k, o in _kuryr_opts]
