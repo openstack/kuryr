0874327d4e0ae9e003c3add3660e70b0833f0d38
Taku Fukushima <f.tac.mac@gmail.com>
2015-12-07 17:45:01 +0900

    Remove unnecessary constants
    This patch removes the unused constants values. Some of them are
    for the old libnetwork API spec and totally obsolete.
    
    Change-Id: I4dd13903cbe9e2059c8bdadf7f13ea42d820b82e
    Signed-off-by: Taku Fukushima <f.tac.mac@gmail.com>
    Closes-Bug: #1523388

--- kuryr/common/constants.py
+++ kuryr/common/constants.py
@@ -11,42 +11,10 @@
 # under the License.
 
 
-TYPES = {
-    # NEXTHOP indicates a StaticRoute with an IP next hop.
-    "NEXTHOP": 0,
-    # CONNECTED indicates a StaticRoute with a interface for directly connected
-    # peers.
-    "CONNECTED": 1
-}
-
 SCHEMA = {
     "PLUGIN_ACTIVATE": {"Implements": ["NetworkDriver"]},
     # TODO(tfukushima): This is mocked and should be replaced with real data.
-    "CREATE_ENDPOINT": {
-        "Interfaces": [{
-            "ID": 1,
-            "Address": "192.168.1.42/24",
-            "AddressIPv6": "fe80::f816:3eff:fe20:57c3/64",
-            "MacAddress": "fa:16:3e:20:57:c3",
-        }]
-    },
-    # TODO(tfukushima): This is mocked and should be replaced with real data.
     "ENDPOINT_OPER_INFO": {"Value": {}},
-    # TODO(tfukushima): This is mocked and should be replaced with real data.
-    "JOIN": {
-        "InterfaceNames": [{
-            "SrcName": "foobar",
-            "DstPrefix": ""
-        }],
-        "Gateway": "192.168.1.1/24",
-        "GatewayIPv6": "fe80::f816:3eff:fe20:57c1/64",
-        "StaticRoutes": [{
-            "Destination": "192.168.1.42",
-            "RouteType": TYPES['CONNECTED'],
-            "NextHop": "",
-            "InterfaceID": 0
-        }]
-    },
     "SUCCESS": {}
 }
 
