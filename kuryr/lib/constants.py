3568412f91aeaf2ce9dd3f269249ff77fb7cefc6
Dongcan Ye <hellochosen@gmail.com>
2016-09-23 18:41:17 +0800

    Remove kuryr-libnetwork constants from kuryr-lib
    Some constants are only used for libnetwork, we can
    remove them from kuryr-lib repo.
    
    Change-Id: Ic2c90427eaf3632eb5f0fd309e057c5517a6e6d9

--- kuryr/lib/constants.py
+++ kuryr/lib/constants.py
@@ -11,20 +11,6 @@
 # under the License.
 
 
-SCHEMA = {
-    "PLUGIN_ACTIVATE": {"Implements": ["NetworkDriver", "IpamDriver"]},
-    # TODO(tfukushima): This is mocked and should be replaced with real data.
-    "ENDPOINT_OPER_INFO": {"Value": {}},
-    "SUCCESS": {}
-}
-
-# Routes are either given a RouteType of 0 and a value for NextHop;
-# or, a RouteType of 1 and no value for NextHop, meaning a connected route.
-ROUTE_TYPE = {
-     "NEXTHOP": 0,
-     "CONNECTED": 1
-}
-
 PORT_STATUS_ACTIVE = 'ACTIVE'
 PORT_STATUS_DOWN = 'DOWN'
 
@@ -32,15 +18,3 @@ DEVICE_OWNER = 'kuryr:container'
 NIC_NAME_LEN = 14
 VETH_PREFIX = 'tap'
 CONTAINER_VETH_PREFIX = 't_c'
-
-NEUTRON_ID_LH_OPTION = 'kuryr.net.uuid.lh'
-NEUTRON_ID_UH_OPTION = 'kuryr.net.uuid.uh'
-NET_NAME_PREFIX = 'kuryr-net-'
-
-REQUEST_ADDRESS_TYPE = 'RequestAddressType'
-NETWORK_GATEWAY_OPTIONS = 'com.docker.network.gateway'
-NETWORK_GENERIC_OPTIONS = 'com.docker.network.generic'
-NEUTRON_UUID_OPTION = 'neutron.net.uuid'
-NEUTRON_NAME_OPTION = 'neutron.net.name'
-KURYR_EXISTING_NEUTRON_NET = 'kuryr.net.existing'
-NEUTRON_POOL_NAME_OPTION = 'neutron.pool.name'
